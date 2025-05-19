9be5b7d5bb77140c41b168f19f8c82f12cde3b85
Aditya Mandhare <adityamandhare47050@gmail.com>
2025-05-19 21:30:57 +0530

    updated backend for image limit size

--- src/index.js
+++ src/index.js
@@ -10,13 +10,17 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 job.start()
-app.use(express.json()); 
-app.use(cors())
+
+// Increase payload size limit here
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/books",bookRoutes);
+app.use("/api/books", bookRoutes);
 
 app.listen(PORT, () => {
-    console.log("Server running at 3000");
+    console.log("Server running at", PORT);
     connectDB();
 });
